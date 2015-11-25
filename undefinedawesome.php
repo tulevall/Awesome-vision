0a90c28194d76c070f3ff67e692284977dc591dd
Frank-Johan <fj.lindholm@gmail.com>
2015-11-25 13:29:13 +0100

    Create undefinedawesome.php

---
+++ undefinedawesome.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Awesome Vision Undefined </title>
+    <link rel="stylesheet" type="text/css" href="XXXX">
+    
+</head>
+<body>   
+    <div class="undefinedbox">
+        <a href="form.php">
+        <img src="settingicon1.png" alt="settings" style="width:42px;height:42px;border:0">
+    
+        </a>
+        <p><b>You have not yet defined what your Awesomeness is!</b></p>
+            <form action="demo_form.asp">
+                <input type="checkbox" name="awesome" value="awesome1"> Category1 <br>
+                <input type="checkbox" name="awesome" value="awesome2"> Category2 <br>
+                <input type="checkbox" name="awesome" value="awesome3"> Category3 <br>   
+                <input type="checkbox" name="awesome" value="awesome4"> Category4 <br>  
+            </form><br>
+        <p><b>To set your Awesome Vision click settings</b></p>
+    </div>
+<?php
+?>       
+</body>
+</html>
